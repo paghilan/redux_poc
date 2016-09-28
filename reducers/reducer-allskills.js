c6abc60840c112181c5577638000833c9b281b37
Ramakrishnan . S <Ramakrishnan.S@nationstarmail.com>
2016-09-28 15:49:47 +0530

    Adding GRADES TO SKILLS

--- reducers/reducer-allskills.js
+++ reducers/reducer-allskills.js
@@ -9,25 +9,25 @@ export default function() {
           id: 1,
           position : "Web Developer",
           skills : [
-              "HTML",
-              "CSS",
-              "JS"
+              {"HTML" : 2} ,
+              {"CSS" : 1},
+              {"JS" : 3},
           ]
       },
       {
           id: 2,
           position : "ETL",
           skills : [
-              "SQL",
-              "Mongo DB"
+              {"SQL" : 3},
+              {"Mongo DB" : 3}
           ]
       },
       {
           id: 3,
           position : "Service Desk",
           skills : [
-              "Operations",
-              "Networking"
+              {"Operations" : 2},
+              {"Networking" : 1}
           ]
       }
   ]
